c0d6359fcf4c03f34b65747044fb2e5db86ac6f0
Pradeep Kolli <pradeepkolli3@yahoo.in>
2020-01-22 22:26:03 -0700

    added code using useState hook

--- src/App.js
+++ src/App.js
@@ -3,15 +3,31 @@ import './App.css';
 import HooksUseState from './Components/HooksUseState';
 import HooksUseStateUsingObject from './Components/HooksUseStateUsingObject';
 import HooksUseStateWithArrays from './Components/HooksUseStateWithArrays';
+import HooksUSeEffect from './Components/HooksUSeEffect';
 
 function App() {
   return (
     <div className="App">
-     <HooksUseState />
-     <br/>
-     <HooksUseStateUsingObject />
-     <br/>
-     <HooksUseStateWithArrays />
+      <div className="Modules">
+        <h3>Hooks using state of type number</h3>
+        <HooksUseState />
+        
+        <br/>
+     </div>
+     <div className="Modules">
+        <h3>Hooks using a state of type object</h3>
+        <HooksUseStateUsingObject />
+        <br/>
+     </div>
+     <div className="Modules">
+        <h3>Hooks using a state of type array</h3>
+        <HooksUseStateWithArrays />
+        <br/>
+     </div>
+     <div className="Modules">
+        <HooksUSeEffect />
+        <br/>
+     </div>
     </div>
   );
 }
---
+++ src/Components/HooksUSeEffect.js
@@ -0,0 +1,16 @@
+import React, { useState , useEffect } from 'react'
+
+function HooksUSeEffect() {
+    const [count, setCount] = useState(0)
+    useEffect(() => {
+        document.title = `Clicked ${count} times`
+    }, [count])
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Count {count}</button>
+            <h3>Watch the tab title change when you click this button</h3>
+        </div>
+    )
+}
+
+export default HooksUSeEffect
